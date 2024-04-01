c5e3b1462ed47eced2c0c1992a2326e5573ff0a7
José Brito <josealbritose@gmail.com>
2024-04-01 13:09:42 -0300

    Alert component

---
+++ src/components/shared/Alert.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Alert({message}:{message:string}) {
+  return (
+    <div className='absolute top-20 border-2 p-2 rounded-lg border-red-300'>
+      <p className='font-bold text-xl'>
+       {message}
+      </p>
+    </div>
+  )
+}
