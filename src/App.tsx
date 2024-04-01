bc9b08018c2a06ab1834cae243a51d4e4e916800
José Brito <josealbritose@gmail.com>
2024-04-01 13:10:16 -0300

    Form rendering

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,19 @@
 import './App.css'
+import Form from './components/Form/Form'
 
 function App() {
 
   return (
-    <main>
-      <h1>Weather App</h1>
+    <main className='flex flex-col justify-center items-center gap-y-10'>
+      <h1 className='text-center font-bold text-4xl mt-8 mb-4'>Weather App</h1>
+      <section className='w-full max-w-[100rem] mx-auto
+        flex flex-col items-center justify-center
+        gap-y-5
+        md:flex-row md:justify-evenly 
+      '>
+        <Form/>
+        <p>h</p>
+      </section>
     </main>
   )
 }
