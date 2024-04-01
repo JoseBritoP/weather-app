1d2064564635844f4730ad3b57bcdd2ed10206ad
José Brito <josealbritose@gmail.com>
2024-04-01 10:16:03 -0300

    First commit

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  darkMode: 'class',
+  theme: {
+    extend: {
+      screens:{
+        sm: '640px',
+        md:'768px',
+        lg:'960px',
+        xl:'1200px',
+        xlg: '1600px'
+      },
+    },
+  },
+  plugins: [],
+}
