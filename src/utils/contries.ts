a32e2c041bf49bfaac55df4a5a3be506e712ffff
José Brito <josealbritose@gmail.com>
2024-04-01 13:09:22 -0300

    Countries utils

---
+++ src/utils/contries.ts
@@ -0,0 +1,32 @@
+import { CountryType } from "../types"
+export const countries:CountryType[] = [
+  {
+    code:'US',
+    name:'United States'
+  },
+  
+  {
+    code:'MX',
+    name:'México'
+  },
+  {
+    code:'AR',
+    name:'Argentina'
+  },
+  {
+    code:'CO',
+    name:'Colombia'
+  },
+  {
+    code:'CR',
+    name:'Costa Rica'
+  },
+  {
+    code:'ES',
+    name:'España'
+  },
+  {
+    code:'PE',
+    name:'Peru'
+  },
+]
